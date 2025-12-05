eb02abbde7dd7d5d7133681fde3f6afa0caa8e27
Jo <johanna.a.k.tjernstrom@gmail.com>
2025-12-05 18:08:34 +0100

    so I think I have an autoencoder now

---
+++ source/anomalyDetector.py
@@ -0,0 +1,61 @@
+import tensorflow as tf
+from tensorflow.keras import layers, losses
+from tensorflow.keras.models import Model
+from sklearn.metrics import accuracy_score, precision_score, recall_score
+import numpy as np
+
+
+class AnomalyDetector(Model):
+    
+    def __init__(self):
+        super(AnomalyDetector,self).__init__()
+        self.encoder = tf.keras.Sequential([
+            layers.Dense(32,activation="relu"),
+            layers.Dense(16,activation="relu"),
+            layers.Dense(8,activation="relu")])
+        
+        self.decoder = tf.keras.Sequential([
+            layers.Dense(16,activation="relu"),
+            layers.Dense(32,activation="relu"),
+            layers.Dense(140,activation="sigmoid")])
+        
+    def call(self,x):
+        encoded = self.encoder(x)
+        decoded = self.decoder(encoded)
+        return decoded
+    
+def anomalyPredict(model: AnomalyDetector, data: np.array, threshold: float):
+    """
+    uses the anomalyDetector model to predict outliers for a given dataset and threshold
+
+    Parameters:
+    -----------
+        model : AnomalyDetector 
+            autoencoder model 
+
+        data : np.array
+            data to detect anomalies in
+
+        threshold : float
+            threshold value 
+    """
+    
+    reconstructions = model(data)
+    loss = tf.keras.mae(reconstructions,data)
+    return tf.math.less(loss,threshold)
+
+def print_stats(predictions,labels):
+    """
+    Prints relevant stats and scores for anomaly classification
+
+    Parameters
+    ----------
+        predictions : array
+            predictions from anomalyPredict
+
+        labels : array
+            test labels for verficiation purposes
+    """
+    print(f"Accuracy = {accuracy_score(labels,predictions)}")
+    print(f"Precision = {precision_score(labels,predictions)}")
+    print(f"Recall = {recall_score(labels,predictions)}")
---
+++ source/data_wrangling.py
@@ -0,0 +1,59 @@
+import numpy as np
+
+def pool_data_array(da):
+    """
+    Function that takes a trajectory based xarray dataset and returns an array
+    (without time or lat/lon indexing)  of all of the values in the dataset. 
+    "pooling" all of the values into one data array.
+
+    Parameters
+    ----------
+
+        da : xr.DataArray
+            array containing the data for the variable you want to pool ds[var name]
+
+    Returns:
+    --------
+
+        pooled data : np.array  
+            array containing all data values for that variable for all trajectories
+    """
+    data_ds_temp = []
+
+    for traj in range(da["trajectory"].size):
+        temp = da.isel(trajectory=traj)
+        temp = temp[~np.isnan(temp)]
+        for val in temp.values:
+            data_ds_temp.append(val)
+
+    return np.array(data_ds_temp) 
+
+def pool_data_variables(ds variables):
+    """
+    Function that takes a trajectory based xarray dataset and returns an array
+    (without time or lat/lon indexing)  of all of the values in the dataset. 
+    "pooling" all of the values into one data array for each of the specified variables, making sure this is
+    done in the correct order and saving the result as a n-d matrix to keep coherence between variable and flags 
+
+    Parameters
+    ----------
+
+        ds : xr.Dataset 
+            array containing the data for the variable you want to pool ds[var name]
+
+    Returns:
+    --------
+
+        pooled data : np.array  
+            array containing all data values for that variable for all trajectories
+            shape number of data points x number variables
+    """
+    data_ds_temp = []
+
+    for traj in range(ds["trajectory"].size):
+        temp = ds.isel(trajectory=traj)
+        temp = temp[~np.isnan(temp)]
+        for val in temp.values:
+            data_ds_temp.append(val)
+
+    return np.array(data_ds_temp)
